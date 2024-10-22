aae6cdbc903bd8c7e97cb30fc0312cf986623967
dGarnett00 <davonta2222@gmail.com>
2024-10-22 10:06:18 -0400

    Added a game

---
+++ saveCode.py
@@ -0,0 +1,99 @@
+''' 
+def decision_timer():
+    for _ in range(10):
+        time.sleep(1)
+        print("", end='', flush=True)
+    print("\nTime's up! You took too long to decide...")
+    return False
+
+
+
+timer_thread_action = threading.Thread(target=decision_timer)
+    timer_thread_action.start()
+    timer_thread_action.join() 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    if doorChoice == "exit":  # Check if user wants to exit the game
+    print("You have chosen to exit the game. Goodbye!")
+    player_health = 0  # End the game
+elif doorChoice == "skull":
+    print("You push open the door with the decaying skull symbol. The air grows colder, and the stench of death fills your lungs."
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    '''
